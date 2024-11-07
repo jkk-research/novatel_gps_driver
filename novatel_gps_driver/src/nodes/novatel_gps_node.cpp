299303eccb2f58e599f1e09fc3565df27441b171
szepilot <szepilot@gmail.com>
2024-11-07 14:36:20 +0100

    90 deg rotation

--- novatel_gps_driver/src/nodes/novatel_gps_node.cpp
+++ novatel_gps_driver/src/nodes/novatel_gps_node.cpp
@@ -874,7 +874,7 @@ namespace novatel_gps_driver
         double P = (msg->pitch) * (M_PI / 180) * -1;
         double Y = (msg->azimuth) * (M_PI / 180) - M_PI;
         oriQuater.setRPY(R,P,Y); 
-        oriRot.setRPY(0.0, 0.0, M_PI);
+        oriRot.setRPY(0.0, 0.0, M_PI/2);
         oriNew = oriRot * oriQuater;
         oriNew.normalize();
         utmpose.pose.orientation.w = oriNew.getW() * -1;
